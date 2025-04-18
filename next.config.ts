4ef3738a8ee42430b9dd4c23379efa80451999cd
Dmytro <2937451+vorotech@users.noreply.github.com>
2025-04-18 14:39:51 +0300

    expose env variable for next

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_CF_PAGES_BRANCH: process.env.CF_PAGES_BRANCH!
+  },
   images: {
     remotePatterns: [
       {
