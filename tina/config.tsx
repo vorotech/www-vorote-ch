c394f60165c983e5b7cb2b1350cb97af5d7d7bfb
Dmytro <2937451+vorotech@users.noreply.github.com>
2025-04-18 14:28:10 +0300

    try to fix way to access env vars

--- tina/config.tsx
+++ tina/config.tsx
@@ -11,7 +11,7 @@ const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
   branch:
     process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-    process.env.CF_PAGES_BRANCH! || // Cloudflare branch env
+    process.env.NEXT_PUBLIC_CF_PAGES_BRANCH! || // Cloudflare branch env
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
     process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
