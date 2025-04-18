742a278fa66578cbf971ba2bea30a80159511b1b
Dmytro <2937451+vorotech@users.noreply.github.com>
2025-04-18 14:46:02 +0300

    revert changes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_CF_PAGES_BRANCH: process.env.CF_PAGES_BRANCH!
-  },
   images: {
     remotePatterns: [
       {
