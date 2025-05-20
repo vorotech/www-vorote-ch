5119dedfbfb8cf63599d6ca10618db18e1eb00d9
Dmytro <2937451+vorotech@users.noreply.github.com>
2025-05-20 19:29:19 +0300

    set mono font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Metadata } from "next";
-import { Inter as FontSans, Lato, Nunito } from "next/font/google";
+import { Abel, Source_Code_Pro } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { VideoDialogProvider } from "@/components/ui/VideoDialogContext";
 import VideoDialog from "@/components/ui/VideoDialog";
@@ -8,19 +8,18 @@ import VideoDialog from "@/components/ui/VideoDialog";
 import "@/styles.css";
 import { TailwindIndicator } from "@/components/ui/breakpoint-indicator";
 
-const fontSans = FontSans({
+
+// Source Code Pro for body text
+const sourceCodePro = Source_Code_Pro({
   subsets: ["latin"],
-  variable: "--font-sans",
+  variable: "--font-source-code-pro",
+  weight: "300",
 });
 
-const nunito = Nunito({
+// Abel for headings
+const abel = Abel({
   subsets: ["latin"],
-  variable: "--font-nunito",
-});
-
-const lato = Lato({
-  subsets: ["latin"],
-  variable: "--font-lato",
+  variable: "--font-abel",
   weight: "400",
 });
 
@@ -35,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn(fontSans.variable, nunito.variable, lato.variable)}>
+    <html lang="en" className={cn(sourceCodePro.variable, abel.variable)}>
       <body className="min-h-screen bg-background font-sans antialiased">
         <VideoDialogProvider>
           {children}
